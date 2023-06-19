e6453a9067d061655a123681036f04bee7d9c201
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 15:09:58 +0800

    Add solution to repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, reps) {
+    if(reps < 0) return "ERROR";
+    let result = "";
+    while(reps > 0){
+        result += str;
+        reps--;
+    }
+    return result;
 };
 
 // Do not edit below this line
