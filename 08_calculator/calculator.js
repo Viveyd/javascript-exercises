26f9e1179266a0e989e00491895999c0155f8297
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 18:00:01 +0800

    Add solution to calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(nArr) {
+  return nArr.reduce((sum, cur) => sum + cur, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...nArr) {
+  return  nArr.reduce((product, cur) => product * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(n, exp) {
+  return n ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n <= 1) return 1;
+	return n * factorial(n-1);
 };
 
 // Do not edit below this line
