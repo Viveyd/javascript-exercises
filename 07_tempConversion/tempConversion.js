5f4456717bdea99432a3fa9c83501302b228b50c
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 17:47:24 +0800

    Add solution to tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Number(((fahrenheit-32) * 0.5556).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Number(((celsius*1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
