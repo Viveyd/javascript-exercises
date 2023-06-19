45601a3335d42ad92262dd27d1e82e0e4f483954
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 15:27:03 +0800

    Add solution to removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...exes) {
+    exes.forEach(ex => {
+        let exIdx = arr.indexOf(ex);
+        if(exIdx == -1) return;
+        arr = [...arr.slice(0, exIdx), ...arr.slice(exIdx+1)]
+    })
+    return arr;
 };
 
 // Do not edit below this line
