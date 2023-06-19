d73eb458821d46cfe9ddd4353e2739e74a29478f
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 17:48:35 +0800

    Edit sumAll.js comment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,8 @@ const sumAll = function(rangeStart, rangeEnd) {
     if(typeof(rangeStart) == "number" && typeof(rangeEnd) == "number" && rangeStart >= 0 && rangeEnd >= 0){
         let [min, max] = rangeStart < rangeEnd ? [rangeStart, rangeEnd]: [rangeEnd, rangeStart];
         // In original formula: sum(1...max) = n(n+1)/2
-        // In modified formula: sum(min...max) = sum(1...max) - sum(1...min).
-        // If min is 1, then follow formula. If not, then use modified formula
+        // In derived formula: sum(min...max) = sum(1...max) - sum(1...min).
+        // If min is 1, then follow original. If not, use modified.
         return min === 1 ? max*(max+1)/2: (max*(max+1)/2) - (min*(min+1)/2);
     }
     return "ERROR";
