fdcba4b4a4bbf57e8da5a11fa8dea87428bf6927
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 18:19:19 +0800

    Add solution to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let valids = "abcdefghijklmnopqrstuvwxyz0123456789";;
+    let cleanStr = str.toLowerCase().split("").filter(char => valids.includes(char));
+    return cleanStr.join("") === cleanStr.reverse().join("");
 };
 
 // Do not edit below this line
