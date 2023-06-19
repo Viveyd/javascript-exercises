015eef738110f8adcae73699c3b2a965bf178d91
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 19:06:34 +0800

    Add fibonacci.js solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    if(n <= 2){
+        return 1;
+    } else{
+        return fibonacci(n-2) + fibonacci(n-1)
+    }
 };
 
 // Do not edit below this line
