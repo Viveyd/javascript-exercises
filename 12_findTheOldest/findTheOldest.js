57f2c9e5689d8efa3f85623377a11a162f15918a
Viveyd <80643437+Viveyd@users.noreply.github.com>
2023-06-19 19:28:56 +0800

    Add findTheOldest.js solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr){
+    return peopleArr.sort((a, b) => getAge(a.yearOfBirth, a.yearOfDeath) - getAge(b.yearOfBirth, b.yearOfDeath))[peopleArr.length-1];
 };
+
+const getAge = function(bd, dd = new Date().getFullYear()){
+    return dd - bd;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
